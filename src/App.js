6e6b948e668c52930fc3d82d102d993ac7eec281
NusantaraProperty23 <140926595+Nusantaraproperty23@users.noreply.github.com>
2023-10-08 23:13:31 +0800

    App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ProductItem from "./components/ProductItem";
-import Axios from "Axios";
+import axios from "axios";
 
 function App() {
   const [userData, setUserData] = useState(null)
