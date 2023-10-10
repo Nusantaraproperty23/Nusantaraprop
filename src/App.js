46b9726cabc9a1e154a68444ff9d2e61db695200
NusantaraProperty23 <140926595+Nusantaraproperty23@users.noreply.github.com>
2023-10-10 22:43:19 +0700

    App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React { useEffect, useState } from "react"
 import ProductItem from "./components/ProductItem"
 import axios from "axios"
 
