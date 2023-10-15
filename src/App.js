5e600ca79aab564be168f42069011a9511c17c5e
NusantaraProperty23 <140926595+Nusantaraproperty23@users.noreply.github.com>
2023-10-15 17:37:11 +0800

    App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react"
 import ProductItem from "./components/ProductItem"
 import axios from "axios"
+import { Empty, Input, Spin } from "antd"
+import { SearchOutlined } from "@ant-design/icons"
 
 function App() {
+  const [loading, setLoading] = useState(false)
   const [userData, setUserData] = useState(null)
   const [products, setProducts] = useState([])
   const config = {
@@ -14,9 +17,33 @@ function App() {
 
   const createLog = (body) => {
     axios
-      .post("https://propertynusantara.stagging.my.id/api/log/create", body)
+      .post("https://propertynusantara.punyakios.com/api/log/create", body)
       .then((res) => {
         console.log("ok")
+      })
+  }
+
+  const loginBE = (body) => {
+    axios
+      .post("https://propertynusantara.punyakios.com/api/auth/login", body)
+      .then((res) => {
+        setUserData(res.data?.data?.data?.user || {})
+        localStorage.setItem(
+          "userDataBe",
+          JSON.stringify(res.data?.data?.data?.user || {})
+        )
+      })
+      .catch((err) => {})
+  }
+
+  const createUserBe = (body) => {
+    axios
+      .post("https://propertynusantara.punyakios.com/api/auth/createUser", body)
+      .then((res) => {
+        // alert("success register")
+      })
+      .catch((err) => {
+        // alert("error register" + JSON.stringify(err.response))
       })
   }
 
@@ -29,14 +56,18 @@ function App() {
     setUserData(authResult)
     localStorage.setItem("userData", JSON.stringify(authResult))
     createLog({ value: "Log Signin", body: JSON.stringify(authResult) })
+    loginBE(authResult.user)
   }
 
   const loadProduct = () => {
+    setLoading(true)
     axios
-      .get("https://propertynusantara.stagging.my.id/api/product/list")
+      .get("https://propertynusantara.punyakios.com/api/product/list")
       .then((res) => {
+        setLoading(false)
         setProducts(res?.data?.data || [])
       })
+      .catch((err) => setLoading(false))
   }
 
   // order
@@ -71,11 +102,13 @@ function App() {
       value: "onReadyForServerApproval",
       body: JSON.stringify({ paymentId }),
     })
-    axios.post(
-      "https://propertynusantara.stagging.my.id/api/payments/approve",
-      { paymentId },
-      config
-    )
+    axios
+      .post(
+        "https://propertynusantara.punyakios.com/api/payments/approve",
+        { paymentId },
+        config
+      )
+      .catch((err) => alert(JSON.stringify(err.response)))
   }
 
   const onReadyForServerCompletion = (paymentId, txid) => {
@@ -86,7 +119,7 @@ function App() {
     })
     // axiosClient.post('/payments/complete', {paymentId, txid}, config);
     axios.post(
-      "https://propertynusantara.stagging.my.id/api/payments/complete",
+      "https://propertynusantara.punyakios.com/api/payments/complete",
       { paymentId, txid },
       config
     )
@@ -98,7 +131,11 @@ function App() {
       value: "onCancel",
       body: JSON.stringify({ paymentId }),
     })
-    // return axiosClient.post("/payments/cancelled_payment", { paymentId })
+    axios.post(
+      "https://propertynusantara.punyakios.com/api/payments/cancel",
+      { paymentId },
+      config
+    )
   }
 
   const onError = (error, payment = null) => {
@@ -118,27 +155,53 @@ function App() {
     loadProduct()
   }, [])
 
-  console.log(userData, "userData")
   return (
-    <div className="max-w-lg mx-auto border-x">
+    <div className="max-w-lg mx-auto border-x h-screen">
       <div className=" bg-white p-4">
-       
+        <div className="flex items-center font-bold text-lg">
+          <img
+            src="https://i.ibb.co/ZcB9pKt/logo.jpg"
+            alt=""
+            className="w-10 h-10 rounded-full"
+          />
+          <p className="ml-3">Property Nusantara</p>
+        </div>
+
+        <div className="mt-8">
+          <Input prefix={<SearchOutlined />} placeholder="cari disini" />
+        </div>
       </div>
-      {userData && <p>{JSON.stringify(userData)}</p>}
-      <div class="grid grid-cols-2 gap-4 mt-8 p-4">
-        {products &&
-          products.map((item) => (
-            <ProductItem
-              key={item.id}
-              item={item}
-              onOrder={() =>
-                orderProduct(item.product_name, 3, { productId: item.id })
-              }
-            />
-          ))}
-      </div>
+      {/* {userData && <p>{JSON.stringify(userData)}</p>} */}
+      {products && products.length > 0 ? (
+        <div class="grid grid-cols-2 gap-4 mt-8 p-4">
+          {products &&
+            products.map((item) => (
+              <ProductItem
+                key={item.id}
+                item={item}
+                onOrder={(value) => {
+                  createUserBe(value)
+                  orderProduct(item.product_name, 3, { productId: item.id })
+                }}
+                user={userData || getItem("userDataBe")}
+              />
+            ))}
+        </div>
+      ) : (
+        <div className="h-[80vh] flex items-center justify-center">
+          {loading ? <Spin /> : <Empty description="Tidak ada produk disini" />}
+        </div>
+      )}
     </div>
   )
+}
+
+const getItem = (key) => {
+  try {
+    return JSON.parse(localStorage.getItem(key))
+  } catch (error) {
+    return localStorage.getItem(key) || {}
+  }
 }
 
 export default App
