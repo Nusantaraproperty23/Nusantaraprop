68ffa967079fc470321c65020dea08cba11e062d
NusantaraProperty23 <140926595+Nusantaraproperty23@users.noreply.github.com>
2024-07-08 09:34:37 +0800

    ProductItem.js

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -7,7 +7,7 @@ const ProductItem = ({ onOrder, item, user }) => {
     <div class=" border rounded-lg">
       <img
         src={
-          item.product_image_url ||
+          item.product_image_urll ||
           "https://www.southernliving.com/thmb/_Msu9OCpvE-OUTRvfYxCIJyPbhE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/27196_AdaptiveCottage00006-2000-7702094217044ce4830a8adb0a69b6da.jpg"
         }
         alt=""
